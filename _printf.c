a4101c1db7671f344310c33370097550b635c22d
oluwagbayilo <princegeeby1@gmail.com>
2023-09-24 19:14:30 +0100

    Update solution

--- _printf.c
+++ _printf.c
@@ -21,80 +21,78 @@
 int _printf(const char *format, ...)
 {
 	int no_of_chars = 0, i = 0, num;
-	char cha, buffing[12];
+	char cha, buffing[12]; /* Buffer for integers */
 	char *strings;
 	va_list args;
 
 	va_start(args, format);
-
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			switch (*format)
 			{
-				case ('c'):
-					{
-						cha = va_arg(args, int);
-						_putchar(cha);
-						no_of_chars++;
-						break;
-					}
-				case ('s'):
-					{
-						strings = va_arg(args, char*);
-						for (i = 0; strings[i] != '\0'; i++)
-						{
-							_putchar(strings[i]);
-							no_of_chars++;
-						}
-						break;
-					}
-				case ('d'):
-				case ('i'):
-					{
-						num = va_arg(args, int);
-						if (num < 0)
-						{
-							_putchar('-');
-							no_of_chars++;
-							num = -num;
-						}
-						i = 0;
-						if (num == 0)
-						{
-							putchar('0');
-							no_of_chars++;
-						}
-						else
-						{
-							while (num > 0)
-						{
-							buffing[i++] = (num % 10) + '0';
-							num = num / 10;
-						}
-						while  (i > 0)
-						{
-							_putchar(buffing[--i]);
-							no_of_chars++;
-						}
-						}
-						break;
-					}
-
-				default:
-					_putchar(*format);
+			case 'c':
+				cha = va_arg(args, int);
+				write(1, &cha, 1);
+				no_of_chars++;
+				break;
+			case 's':
+				strings = va_arg(args, char *);
+				while (strings[i])
+				{
+					write(1, &strings[i], 1);
 					no_of_chars++;
+					i++;
+				}
+				i = 0;
+				break;
+			case 'd':
+			case 'i':
+				num = va_arg(args, int);
+				if (num < 0)
+				{
+					write(1, "-", 1);
+					no_of_chars++;
+					num = -num;
+				}
+				if (num == 0)
+				{
+					write(1, "0", 1);
+					no_of_chars++;
+				}
+				i = 0;
+				while (num > 0)
+				{
+					buffing[i++] = (num % 10) + '0';
+					num /= 10;
+				}
+				while (i > 0)
+				{
+					i--;
+					write(1, &buffing[i], 1);
+					no_of_chars++;
+				}
+				break;
+			default:
+				write(1, "%", 1);
+				no_of_chars++;
+				if (*format)
+				{
+					write(1, format, 1);
+					no_of_chars++;
+				}
 			}
 		}
 		else
 		{
-			_putchar(*format);
+			write(1, format, 1);
 			no_of_chars++;
 		}
 		format++;
 	}
 	va_end(args);
 	return (no_of_chars);
+
 }
